0f2665f9d96b318fa852b47157f3c10b1a5c3081
Ivano Donadi <ivadonadi98@gmail.com>
2017-03-14 22:12:59 +0100

    Static compressing is now working, next is static decompressing
    Former-commit-id: e474339e616a3223b9847dd29a8f1dabb8fe1761 [formerly bb85b3e6654bb00f98b73bb1fdeb4317ce94a83c]
    Former-commit-id: 8fed5ce88630c4e2fa309bc99a8c1d3255116a28

--- DEFLATE/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "deflate_encoder.h"
-
-int main(int argc, char** argv) {
-	Deflate_encoder encoder = Deflate_encoder("Deflate late");
-	encoder.encode();
-	system("Pause");
-}
