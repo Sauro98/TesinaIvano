4de38fea32931e9e488f2b1be207c4a680987f14
Ivano Donadi <ivadonadi98@gmail.com>
2017-04-28 19:19:39 +0200

    Trying to optimize lz77
    Former-commit-id: 7ee4de4de4fef08945e4234f6ee18008d43effad [formerly 3afb05d2f028d022cd67bef03a35c78945fb146a] [formerly fd2710405ec4113e688f456915e85920c10a96b3 [formerly 22b8a47418f28fe5197fda1789188c1f61bb44a3]]
    Former-commit-id: 5f73e930d7e31f23500adbebce5de598c631803a [formerly 4a9c9eab97acdb68606c312cb4c3273da2ca2616]
    Former-commit-id: bef8fc85b137bedd1498e0a16f7412e8ffcb934c

--- LZ77/main.cpp
+++ LZ77/main.cpp
@@ -1,13 +1,28 @@
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include "lz77_encoder.h"
 int main(int argc, char *argv[])
 {
+	std::ifstream input( "gym.blend", std::ios::binary );
+    // copies all data into buffer
+    std::vector<char> buffer((
+            std::istreambuf_iterator<char>(input)), 
+            (std::istreambuf_iterator<char>()));
+	
+	std::string orig(buffer.begin(),buffer.end());
+	input.close();
     LZ77_Encoder encoder = LZ77_Encoder();
     int encoded_length;
-    lz77_r* encoded = encoder.encode("Deflate and inflate until it is late and eat on a plate",&encoded_length);
+    //std::string orig = "Deflate and inflate until it is late and eat on a plate";
+    lz77_r* encoded = encoder.encode(orig,&encoded_length);
     println("ENCODED DONE");
     std::string s = encoder.decode(encoded,encoded_length);
-    system("PAUSE");
+    if(orig == s)
+    	println("SUCCESS");
+    else
+    	println("FAILURE");
+	//system("PAUSE");
     return 0;
 }
