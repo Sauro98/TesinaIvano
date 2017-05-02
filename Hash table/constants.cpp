2b9755056a1fd30c1c9219a0a854b2c275d2045b
Ivano Donadi <ivadonadi98@gmail.com>
2017-05-02 19:58:46 +0200

    Hash tables working
    Former-commit-id: e5c998662fef8fc6b7600ef38135a0be8256106f [formerly b5abd2cf06bf834053efc5528bbed4f755b31eba] [formerly 3125570e486f9896e1a31fea0201135cdd2bbeee [formerly 27ac12267007a9ae918596225debccbd7ecd8101]]
    Former-commit-id: 5e095a1301b6c043ec1a885961af3d62dcde6f3a [formerly 969f49242bd6c0d8e3cda281e1a5044856285e2d]
    Former-commit-id: 17cebb60d33da290d673f42bd50e31af9cceffba

--- Hash table/constants.cpp
+++ Hash table/constants.cpp
@@ -1,24 +1,17 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#include <stdlib.h>
-#include <iostream>
-#include <string.h>
+#include "linked_list.h"
 
-#define print(x) std::cout<<x;
-#define println(x) std::cout<<x<<std::endl;
+
 
 typedef struct item{
 	char* key;
-	int value;
-	
-	item(char* _key,int _value){
-		key = _key;
-		value = _value;
-	}
+	Linked_list value;
 	
 	item(){
 		key = NULL;
+		value = Linked_list();
 	}
 	
 }item;
